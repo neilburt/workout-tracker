5108ace375b47f7a73edd4f098574c2928f6764b
neilburt <neil.burt@comcast.net>
2021-08-04 17:27:00 -0500

    Commenting and minor refactoring.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,56 +1,56 @@
 const router = require('express').Router();
 const Workout = require('../models/workoutModel');
 
+// route that allows for creating new workout
 router.post('/api/workouts', ({body}, res) => {
-  console.log("create hit");
   Workout.create(body)
   .then(dbWorkout => {
     res.json(dbWorkout);
   })
   .catch(err => {
-    console.log("create workout", err)
+    console.log("create workout", err);
     res.status(400).json(err);
   });
 });
 
+// route that allows for adding an exercise to a workout
 router.put('/api/workouts/:id', ({params, body}, res) => {
-  console.log("by id hit");
   Workout.findOneAndUpdate(
     {_id: params.id},
     { $push: { exercises: body } },
     { new: true })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      console.log("by id", err)
-      res.status(400).json(err);
-    });
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    console.log("by id", err);
+    res.status(400).json(err);
   });
-  
-  router.get('/api/workouts', (req, res) => {
-    console.log("range hit");
-    Workout.aggregate([{$addFields: {'totalDuration': {$sum: '$exercises.duration'}}}])
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      console.log("get range", err)
-      res.status(400).json(err);
-    });
-  });
+});
 
-  router.get('/api/workouts/range', (req, res) => {
-    console.log("range route hit");
-    Workout.aggregate([{$addFields: {'totalDuration': {$sum: '$exercises.duration'}}}])
-    .sort({_id: -1}).limit(7)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      console.log("get range", err)
-      res.status(400).json(err);
-    });
+// route that allows for displaying latest workout information
+router.get('/api/workouts', (req, res) => {
+  Workout.aggregate([{$addFields: {'totalDuration': {$sum: '$exercises.duration'}}}])
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    console.log("get range", err);
+    res.status(400).json(err);
   });
+});
+
+// route that allows for populating dashboard charts
+router.get('/api/workouts/range', (req, res) => {
+  Workout.aggregate([{$addFields: {'totalDuration': {$sum: '$exercises.duration'}}}])
+  .sort({_id: -1}).limit(7)
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    console.log("get range", err);
+    res.status(400).json(err);
+  });
+});
   
   module.exports = router;
